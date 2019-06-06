f2f6c355cd099d3c35552aa7226bf69773ae019a
BigBricks <bsa6.23.94@gmail.com>
2019-06-06 14:36:40 -0400

    iq test

---
+++ IqTest.py
@@ -0,0 +1,6 @@
+def iq_test(numbers):
+    #your code here
+    numbers = numbers.split(" ")
+    even = [w for w in range(len(numbers)) if int(numbers[w]) % 2 == 0]
+    odd = [w for w in range(len(numbers)) if int(numbers[w]) % 2 != 0]
+    return odd[0] + 1 if len(odd) < len(even) else even[0] + 1
