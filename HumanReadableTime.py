4cabb1a0cf1a95925074b65649cc04c6a49b3cd5
BigBricks <bsa6.23.94@gmail.com>
2019-06-12 23:35:04 -0400

    human readable time

---
+++ HumanReadableTime.py
@@ -0,0 +1,14 @@
+import math
+def make_readable(seconds):
+    # Do something
+    hours = math.floor(seconds/3600)
+    seconds = seconds - (hours*3600)
+    minutes = math.floor(seconds/60)
+    seconds = seconds - (minutes*60)
+    return '{}:{}:{}'.format(add_zero(hours), add_zero(minutes), add_zero(seconds))
+    
+def add_zero(number):
+    if number >= 10:
+        return str(number)
+    else:
+        return "0" + str(number)
